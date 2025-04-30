2b957871bd073d9893c015aeb102b885366a8ef1
Flugel <151924761+Eu4ngelion@users.noreply.github.com>
2025-04-30 19:46:28 +0800

    Delete Posttest6 directory

--- Posttest6/src/kos/bangunan.java
+++
@@ -1,8 +0,0 @@
-package kos;
-
-abstract  interface bangunan {
-    abstract  int getId();
-    abstract  void setId(int id);
-    abstract  int getHarga();
-    abstract  void setHarga(int harga);
-}
